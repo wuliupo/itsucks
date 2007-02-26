1a0e1e5e16ed194953d8a6963cac8f4a5843c11b
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2007-02-26 22:54:26 +0000

    changed to an interface
    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@67 540001c9-3a26-0410-bb9e-87544be22eba

--- itsucks/core/src/main/java/de/phleisch/app/itsucks/io/DataRetriever.java
+++ itsucks/core/src/main/java/de/phleisch/app/itsucks/io/DataRetriever.java
@@ -1,51 +1,48 @@
 /* Copyright (C) 2006-2007 Oliver Mihatsch (banishedknight@users.sf.net)
  * This is free software distributed under the terms of the
  * GNU Public License.  See the file COPYING for details. 
- * 
+ *
  * $Id$
+ * Created on 26.02.2007
  */
 
 package de.phleisch.app.itsucks.io;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Observable;
+import java.util.Observer;
 
+public interface DataRetriever {
 
-public abstract class DataRetriever extends Observable {
-
-	protected List<DataProcessor> mDataProcessors = new ArrayList<DataProcessor>();
-	protected URL mUrl;
-	
 	/**
 	 * This type of notification will be send if the progress changes.
 	 */
 	public final static Integer NOTIFICATION_PROGRESS = 100;
-	
-	public DataRetriever() {
-		super();
-	}
-	
-	public void addDataProcessor(DataProcessor pDataProcessor) {
-		mDataProcessors.add(pDataProcessor);
-	}
 
-	public URL getUrl() {
-		return mUrl;
-	}
+	public void addDataProcessor(DataProcessor pDataProcessor);
 
-	public void setUrl(URL pUrl) {
-		mUrl = pUrl;
-	}
+	public URL getUrl();
 
-	protected abstract void connect() throws Exception;
-	protected abstract boolean isDataAvailable() throws Exception;
-	protected abstract void retrieve() throws Exception;
-	protected abstract void disconnect() throws Exception;
+	public void setUrl(URL pUrl);
+
+	public void connect() throws Exception;
+
+	public boolean isDataAvailable() throws Exception;
+
+	public void retrieve() throws Exception;
+
+	public void disconnect() throws Exception;
+
+	public long getBytesDownloaded();
+
+	public float getProgress();
+
+	public void abort();
+
+	public Metadata getMetadata();
+
 	
-	protected abstract long getBytesDownloaded();
-	protected abstract float getProgress();
+	//Observable interface
 	
-	public abstract Metadata getMetadata();
+	public void addObserver(Observer o);
+	public void deleteObserver(Observer o);
 }
