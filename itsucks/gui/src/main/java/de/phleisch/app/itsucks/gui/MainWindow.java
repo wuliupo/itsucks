40300f3c5b6d3c871302f2bae84b3f15a7c1677e
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2007-03-08 23:05:39 +0000

    small fix
    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@87 540001c9-3a26-0410-bb9e-87544be22eba

--- itsucks/gui/src/main/java/de/phleisch/app/itsucks/gui/MainWindow.java
+++ itsucks/gui/src/main/java/de/phleisch/app/itsucks/gui/MainWindow.java
@@ -16,8 +16,6 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -495,10 +493,10 @@ public class MainWindow implements AddDownloadJobInterface {
 				jobList = serializationManager.deserialize(fc.getSelectedFile());
 			} catch (Exception e1) {
 				
-				mLog.error("Error occured while saving download template", e1);
+				mLog.error("Error occured while loading download template", e1);
 				
 				JOptionPane.showMessageDialog(jFrame, 
-						"Error occured while saving download template.\n" + e1.getMessage(), 
+						"Error occured while loading download template.\n" + e1.getMessage(), 
 						"Error occured", JOptionPane.ERROR_MESSAGE );
 			} 
 			
