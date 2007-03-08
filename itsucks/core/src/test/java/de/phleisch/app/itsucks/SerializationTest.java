ff70c4538b443e489d0e38fb00e9a7214e2a3544
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2007-03-08 23:04:41 +0000

    initial
    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@85 540001c9-3a26-0410-bb9e-87544be22eba

---
+++ itsucks/core/src/test/java/de/phleisch/app/itsucks/SerializationTest.java
@@ -0,0 +1,53 @@
+/* Copyright (C) 2006-2007 Oliver Mihatsch (banishedknight@users.sf.net)
+ * This is free software distributed under the terms of the
+ * GNU Public License.  See the file COPYING for details. 
+ *
+ * $Id$
+ * Created on 07.03.2007
+ */
+
+package de.phleisch.app.itsucks;
+
+import java.io.File;
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import de.phleisch.app.itsucks.filter.DownloadJobFilter;
+import de.phleisch.app.itsucks.io.DownloadJob;
+import de.phleisch.app.itsucks.persistence.JobSerializationManager;
+import de.phleisch.app.itsucks.persistence.SerializableJobList;
+
+public class SerializationTest extends TestCase {
+	
+	public void testSerialization() throws Exception {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(ApplicationConstants.CORE_SPRING_CONFIG_FILE);
+		
+		DownloadJobFilter filter = (DownloadJobFilter) context.getBean("DownloadJobFilter");
+		filter.setAllowedHostNames(new String[] {".*"});
+		filter.setMaxRecursionDepth(1);
+		filter.setSaveToFileFilter(new String[] {".*[Jj][Pp][Gg]", ".*[Pp][Nn][Gg]", ".*[Gg][Ii][Ff]"});
+		
+		DownloadJob job = (DownloadJob) context.getBean("DownloadJob");
+		job.setUrl(new URL("http://itsucks.sourceforge.net/"));
+		job.setSavePath(new File("/tmp/itsucks"));
+		
+		SerializableJobList serializedObject = new SerializableJobList();
+		serializedObject.addFilter(filter);
+		serializedObject.addJob(job);
+		
+		File file = File.createTempFile("itsucks_junit_", "_test");
+		
+		JobSerializationManager serializator = (JobSerializationManager) context.getBean("JobSerializationManager");
+		serializator.serialize(serializedObject, file);
+		
+		SerializableJobList deserializedList = serializator.deserialize(file);
+		assertTrue(((DownloadJob)deserializedList.getJobs().get(0)).getUrl().equals(job.getUrl()));
+		assertTrue(((DownloadJobFilter)deserializedList.getFilters().get(0)).getSaveToFileFilter()[0].equals(filter.getSaveToFileFilter()[0]));
+
+		file.delete();
+	}
+}
