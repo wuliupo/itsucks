1dec30ccb4062d510d12d6acd0381e3469175142
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2007-03-29 20:35:05 +0000

    update
    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@114 540001c9-3a26-0410-bb9e-87544be22eba

--- itsucks-website/screenshots.php
+++ itsucks-website/screenshots.php
@@ -5,7 +5,28 @@ include_once('include/header.php.inc');
 ?>
 
     <div class="main">
-      <p class="text"><h3>Coming soon...</h3></p>
+  
+       <div style="margin:1em 1em 1em 1em; text-align:center;">
+         <h3>Main window + add download window</h3>
+         <a href="img/screenshot-main-0.2.0-pre6.png">
+            <img style="vertical-align:middle;" src="img/thmb-screenshot-main-0.2.0-pre6.png"/>
+         </a>
+       </div>
+         
+       <div style="margin: 1em 1em 1em 1em; text-align:center;">
+         <h3>Add advanced filter window</h3>
+         <a href="img/screenshot-add_advanced_filter-0.2.0-pre6.png">
+            <img src="img/thmb-screenshot-add_advanced_filter-0.2.0-pre6.png"/>
+         </a>
+       </div>
+       
+       <div style="margin: 1em 1em 1em 1em; text-align:center;">
+         <h3>Regular expression editor</h3>
+         <a href="img/screenshot-edit_regular_expression-0.2.0-pre6.png">
+            <img src="img/thmb-screenshot-edit_regular_expression-0.2.0-pre6.png"/>
+         </a>
+       </div>
+    
     </div>
     
 <?
