55162cf7e49be4555f74a87a6c994d2cc75ed587
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2007-03-31 19:18:02 +0000

    Initial import.
    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@116 540001c9-3a26-0410-bb9e-87544be22eba

---
+++ itsucks-console/src/main/java/de/phleisch/app/itsucks/console/ConsoleMain.java
@@ -0,0 +1,75 @@
+package de.phleisch.app.itsucks.console;
+
+import java.io.File;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import de.phleisch.app.itsucks.ApplicationConstants;
+import de.phleisch.app.itsucks.Dispatcher;
+import de.phleisch.app.itsucks.Job;
+import de.phleisch.app.itsucks.persistence.JobSerializationManager;
+import de.phleisch.app.itsucks.persistence.SerializableJobList;
+
+public class ConsoleMain {
+
+	private static Log mLog = LogFactory.getLog(ConsoleMain.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] pArgs) {
+
+		if(pArgs.length == 0) {
+			showUsage();
+			System.exit(1);
+		}
+		
+		File serializedJob = new File(pArgs[0]);
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				ApplicationConstants.CORE_SPRING_CONFIG_FILE);
+
+		Dispatcher dispatcher = (Dispatcher) context.getBean("Dispatcher");
+
+		JobSerializationManager serializationManager = (JobSerializationManager) context
+				.getBean("JobSerializationManager");
+
+		SerializableJobList jobList = null;
+		try {
+			jobList = serializationManager.deserialize(serializedJob);
+		} catch (Exception e1) {
+			mLog.error("Error occured while loading job template", e1);
+			System.exit(1);
+		}
+
+		if (jobList != null) {
+			
+			dispatcher.addJobFilter(jobList.getFilters());
+			for (Job job : jobList.getJobs()) {
+				dispatcher.addJob(job);
+			}
+		}
+		
+		//start dispatcher thread
+		try {
+			dispatcher.processJobs();
+		} catch (Exception e) {
+			mLog.error("Error starting dispatcher thread", e);
+		}
+		
+	}
+
+	private static void showUsage() {
+		
+		StringBuilder usage = new StringBuilder();
+		
+		usage.append("Usage: java -jar itsucks-console.jar <serialized job template>" + '\n');
+		usage.append("   Starts processing of the jobs in the job list." + '\n');
+		usage.append('\n');
+		
+		System.out.println(usage);
+	}
+
+}
