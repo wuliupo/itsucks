5de75a2360703d4e7a495612b6d16647a4945e5c
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2007-04-08 22:19:24 +0000

    update
    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@129 540001c9-3a26-0410-bb9e-87544be22eba

---
+++ itsucks-core/src/main/java/de/phleisch/app/itsucks/WorkerPool.java
@@ -0,0 +1,242 @@
+/* Copyright (C) 2006-2007 Oliver Mihatsch (banishedknight@users.sf.net)
+ * This is free software distributed under the terms of the
+ * GNU Public License.  See the file COPYING for details. 
+ * 
+ * $Id: WorkerPool.java,v 1.1 2006-12-03 19:31:43 olli Exp $
+ * Created on 03.03.2006
+ */
+
+package de.phleisch.app.itsucks;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class WorkerPool implements IWorkerPool {
+
+	private static final int DEFAULT_POOL_SIZE = 3;
+
+	private static Log mLog = LogFactory.getLog(WorkerThread.class);
+	
+	private Stack<WorkerThread> mFreeWorker = new Stack<WorkerThread>();
+	private List<WorkerThread> mBusyWorker = new ArrayList<WorkerThread>();
+	private int mWorkerIdSequence = 0;
+	
+	private boolean mInitialized;
+	private int mDesiredSize;
+	private int mRealSize;
+	
+	public WorkerPool() {
+		this(DEFAULT_POOL_SIZE);
+	}
+	
+	public WorkerPool(int pSize) {
+		super();
+		mRealSize = 0; 
+		mDesiredSize = pSize;
+		mInitialized = false;
+	}
+	
+	/* (non-Javadoc)
+	 * @see de.phleisch.app.itsucks.IWorkerPool#initialize()
+	 */
+	public synchronized void initialize() {
+		if(mInitialized) return;
+		
+		updateSize();
+		mInitialized = true;
+	}
+	
+	/* (non-Javadoc)
+	 * @see de.phleisch.app.itsucks.IWorkerPool#shutdown()
+	 */
+	public synchronized void shutdown() {
+		if(!mInitialized) return; 
+		
+		decreasePool(mRealSize);
+		mRealSize = mFreeWorker.size() + mBusyWorker.size();
+		
+		if(mRealSize > 0) {
+			throw new IllegalStateException("Could not free all worker threads!");
+		}
+		
+		mInitialized = false;
+		
+		//notify all threads waiting for an free worker
+		this.notify();
+	}
+	
+	/**
+	 * Checks if an pool resize is necessary.
+	 */
+	private synchronized void updateSize() {
+		
+		if(mRealSize != mDesiredSize) {
+			
+			if(mDesiredSize > mRealSize) {
+				//real size is too small, increase to desired size
+				increasePool(mDesiredSize - mRealSize);
+			} else {
+				//real size is too big, try to decrease to desired size
+				decreasePool(mRealSize - mDesiredSize);
+			}
+		}
+		
+		mRealSize = mFreeWorker.size() + mBusyWorker.size();
+		
+		this.notify();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.phleisch.app.itsucks.IWorkerPool#setSize(int)
+	 */
+	public synchronized void setSize(int pSize) {
+		
+		if(pSize == mDesiredSize) return;
+		mDesiredSize = pSize;
+		
+		updateSize();
+	}
+	
+	
+	/**
+	 * Increases the pool by the given size.
+	 * @param pCount
+	 */
+	private synchronized void increasePool(int pCount) {
+		mLog.info("Increase worker pool by " + pCount + " elements.");
+		
+		for (int i = 0; i < pCount; i++) {
+			WorkerThread worker = new WorkerThread(this, "Worker: " + mWorkerIdSequence ++);
+			worker.start();
+			mFreeWorker.push(worker);
+		}
+	}
+	
+	/**
+	 * Tries to shrink the pool by the given size.
+	 * @param pCount
+	 * @return
+	 */
+	private synchronized int decreasePool(int pCount) {
+		mLog.info("Decrease worker pool by " + pCount + " elements.");
+		
+		int freedElements = 0;
+		
+		for(Iterator<WorkerThread> it = mFreeWorker.iterator(); it.hasNext(); ) {
+			
+			if(freedElements >= pCount) break;
+			
+			WorkerThread workerThread = it.next();
+			stopWorker(workerThread);
+			it.remove();
+			
+			freedElements ++;
+		}
+		
+		return freedElements;
+	}
+
+	/**
+	 * Stops the given the worker and wait for the thread to end.
+	 * @param pWorkerThread
+	 */
+	private void stopWorker(WorkerThread pWorkerThread) {
+		
+		pWorkerThread.addCommand(WorkerThread.CMD_SHUTDOWN);
+		try {
+			pWorkerThread.join();
+		} catch (InterruptedException e) {
+			mLog.error("Got interrupted while waiting for worker thread to join!", e);
+			throw new RuntimeException("Got interrupted while waiting for worker thread to join!", e);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see de.phleisch.app.itsucks.IWorkerPool#getFreeWorker()
+	 */
+	public synchronized WorkerThread getFreeWorker() {
+		WorkerThread worker = null;
+		
+		if(!mInitialized) {
+			throw new RuntimeException("Pool not initialized!");
+		}
+		
+		if(mFreeWorker.size() > 0) {
+			worker = mFreeWorker.pop();
+			mLog.debug("Move worker into busy list: " + worker);
+			mBusyWorker.add(worker);
+		}
+		
+		return worker;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.phleisch.app.itsucks.IWorkerPool#returnWorker(de.phleisch.app.itsucks.WorkerThread)
+	 */
+	public synchronized void returnWorker(WorkerThread pWorker) {
+		mLog.debug("Return worker into free list: " + pWorker);
+		
+		if(mBusyWorker.contains(pWorker)) {
+			mBusyWorker.remove(pWorker);
+			mFreeWorker.push(pWorker);
+		} else {
+			throw new IllegalArgumentException("Worker is not known as busy!");
+		}
+
+		updateSize();
+	}
+	
+	/* (non-Javadoc)
+	 * @see de.phleisch.app.itsucks.IWorkerPool#waitForFreeWorker()
+	 */
+	public WorkerThread waitForFreeWorker() throws InterruptedException {
+		
+		WorkerThread worker = getFreeWorker();
+		
+		while(worker == null) {
+			
+			synchronized (this) {
+				//wait for an change in the worker pool
+				this.wait();
+			}
+			worker = getFreeWorker();
+		}
+		
+		return worker;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.phleisch.app.itsucks.IWorkerPool#dispatchJob(de.phleisch.app.itsucks.Job)
+	 */
+	public void dispatchJob(Job job) throws InterruptedException {
+		WorkerThread worker = waitForFreeWorker();
+		
+		job.setState(Job.STATE_ASSIGNED);
+		worker.setJob(job);
+		worker.addCommand(WorkerThread.CMD_PROCESS_JOB);
+		worker.addCommand(WorkerThread.CMD_RETURN_TO_POOL);
+	}
+	
+	/* (non-Javadoc)
+	 * @see de.phleisch.app.itsucks.IWorkerPool#getBusyWorkerCount()
+	 */
+	public int getBusyWorkerCount() {
+		return mBusyWorker.size();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.phleisch.app.itsucks.IWorkerPool#abortBusyWorker()
+	 */
+	public synchronized void abortBusyWorker() {
+		
+		for (WorkerThread worker : mBusyWorker) {
+			worker.abort();
+		}	
+		
+	}
+}
