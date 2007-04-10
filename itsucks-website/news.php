7e865af77a0334147805f9d54ac9b023cb488907
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2007-04-10 13:21:43 +0000

    update
    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@138 540001c9-3a26-0410-bb9e-87544be22eba

--- itsucks-website/news.php
+++ itsucks-website/news.php
@@ -7,6 +7,24 @@ include_once('include/header.php.inc');
     <div class="main">
 
       <p class="text">
+        <h2>Version 0.2.0-pre8 is released</h2>
+        <div class='date'>2007-04-10</div>
+        <p>
+		The most important new features are the pause feature and that advanced filter are 
+		re-editable after creating them. Also very interesting is the new console interface. 
+		Create your download using the GUI and save it as download template. 
+		The download can be started later on the console with 'java -jar itsucks-console-0.2.0-pre8.jar &lt;download template&gt;'. 
+		Many bugfixes are included as well.
+		</p>
+		<p>
+		The project is slowly nearing beta status, so please post any bug/issue you find in the forum.
+		</p>
+		<p>
+		Detailed changelog can be found here: <a href="http://sourceforge.net/project/shownotes.php?release_id=500059&group_id=186141"> ChangeLog 0.2.0-pre8</a>
+		</p>
+      </p>
+
+      <p class="text">
         <h2>Version 0.2.0-pre7 is released</h2>
         <div class='date'>2007-03-31</div>
         Majorly an bugfix release. New is the pause function and an console only util.
