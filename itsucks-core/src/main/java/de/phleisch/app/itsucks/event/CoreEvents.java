3db78bae5a01a0c911868a82c7c2715ed1622332
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2007-05-03 17:08:33 +0000

    update
    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@151 540001c9-3a26-0410-bb9e-87544be22eba

--- itsucks-core/src/main/java/de/phleisch/app/itsucks/event/CoreEvents.java
+++ itsucks-core/src/main/java/de/phleisch/app/itsucks/event/CoreEvents.java
@@ -30,34 +30,44 @@ public class CoreEvents {
 		
 	}
 	
-	public final static int EVENT_CATEGORY_CORE 		= 100;
-	public final static int EVENT_CATEGORY_JOBMANAGER 	= 200;
-	public final static int EVENT_CATEGORY_JOB 			= 300;
+	public final static int EVENT_CATEGORY_MANAGER_CMD	= 100;
+	
+	public final static int EVENT_CATEGORY_CORE 		= 200;
+	public final static int EVENT_CATEGORY_JOBMANAGER 	= 300;
+	public final static int EVENT_CATEGORY_JOB 			= 400;
 	
 	
 	public final static ConstEvent 
-		EVENT_DISPATCHER_START = new ConstEvent(1001, EVENT_CATEGORY_CORE);
+		EVENT_MANAGER_STARTUP = new ConstEvent(1001, EVENT_CATEGORY_MANAGER_CMD);
+	
+	public final static ConstEvent 
+		EVENT_MANAGER_SHUTDOWN = new ConstEvent(1002, EVENT_CATEGORY_MANAGER_CMD);
+	
+	
+	public final static ConstEvent 
+		EVENT_DISPATCHER_START = new ConstEvent(2001, EVENT_CATEGORY_CORE);
 
 	public final static ConstEvent 
-		EVENT_DISPATCHER_PAUSE = new ConstEvent(1002, EVENT_CATEGORY_CORE);
+		EVENT_DISPATCHER_PAUSE = new ConstEvent(2002, EVENT_CATEGORY_CORE);
 
 	public final static ConstEvent 
-		EVENT_DISPATCHER_UNPAUSE = new ConstEvent(1003, EVENT_CATEGORY_CORE);
+		EVENT_DISPATCHER_UNPAUSE = new ConstEvent(2003, EVENT_CATEGORY_CORE);
 
 	public final static ConstEvent 
-		EVENT_DISPATCHER_FINISH = new ConstEvent(1004, EVENT_CATEGORY_CORE);
+		EVENT_DISPATCHER_FINISH = new ConstEvent(2004, EVENT_CATEGORY_CORE);
 
 	
 	public final static ConstEvent 
-		EVENT_JOBMANAGER_JOB_ADDED = new ConstEvent(2001, EVENT_CATEGORY_JOBMANAGER);
+		EVENT_JOBMANAGER_JOB_ADDED = new ConstEvent(3001, EVENT_CATEGORY_JOBMANAGER);
 	
 	public final static ConstEvent 
-		EVENT_JOBMANAGER_JOB_FILTERED = new ConstEvent(2002, EVENT_CATEGORY_JOBMANAGER);
+		EVENT_JOBMANAGER_JOB_FILTERED = new ConstEvent(3002, EVENT_CATEGORY_JOBMANAGER);
 
 	public final static ConstEvent 
-		EVENT_JOBMANAGER_JOB_REMOVED = new ConstEvent(2003, EVENT_CATEGORY_JOBMANAGER);
+		EVENT_JOBMANAGER_JOB_REMOVED = new ConstEvent(3003, EVENT_CATEGORY_JOBMANAGER);
+	
 	
 	public final static ConstEvent 
-		EVENT_JOB_CHANGED = new ConstEvent(3002, EVENT_CATEGORY_JOB);
+		EVENT_JOB_CHANGED = new ConstEvent(4002, EVENT_CATEGORY_JOB);
 	
 }
