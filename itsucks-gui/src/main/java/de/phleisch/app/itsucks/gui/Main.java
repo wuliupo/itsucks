f79bdd2a31838e76649fce5c5e15ae8c19da3f30
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2007-10-10 20:39:20 +0000

    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@313 540001c9-3a26-0410-bb9e-87544be22eba

---
+++ itsucks-gui/src/main/java/de/phleisch/app/itsucks/gui/Main.java
@@ -0,0 +1,39 @@
+/* Copyright (C) 2006-2007 Oliver Mihatsch (banishedknight@users.sf.net)
+ * This is free software distributed under the terms of the
+ * GNU Public License.  See the file COPYING for details. 
+ * 
+ * $Id$
+ */
+
+package de.phleisch.app.itsucks.gui;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+	/**
+	 * Launches this application
+	 */
+	public static void main(String[] args) {
+		
+		try {
+			//UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+			//UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			//UIManager.setLookAndFeel("com.jgoodies.looks.plastic.PlasticXPLookAndFeel");
+			//UIManager.setLookAndFeel("com.jgoodies.looks.plastic.Plastic3DLookAndFeel");
+			//UIManager.setLookAndFeel("com.jgoodies.looks.plastic.PlasticLookAndFeel");
+			//UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				DownloadJobOverviewFrame frame = new DownloadJobOverviewFrame();
+				frame.setVisible(true);
+			}
+		});
+	}
+
+}
