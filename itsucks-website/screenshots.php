25429e2654e3affc3417159b609524f3def735ec
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2007-10-27 11:34:54 +0000

    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@335 540001c9-3a26-0410-bb9e-87544be22eba

--- itsucks-website/screenshots.php
+++ itsucks-website/screenshots.php
@@ -8,22 +8,29 @@ include_once('include/header.php.inc');
   
        <div style="margin:1em 1em 1em 1em; text-align:center;">
          <h3>Main window + add download window</h3>
-         <a href="img/screenshot-main-0.2.0-pre6.png">
-            <img style="vertical-align:middle;" src="img/thmb-screenshot-main-0.2.0-pre6.png"/>
+         <a href="img/screenshot-main-0.3.0-pre2.png">
+            <img style="vertical-align:middle;" src="img/thmb-screenshot-main-0.3.0-pre2.png"/>
+         </a>
+       </div>
+       
+       <div style="margin: 1em 1em 1em 1em; text-align:center;">
+         <h3>Edit simple rules window</h3>
+         <a href="img/screenshot-simple_rules-0.3.0-pre2.png">
+            <img src="img/thmb-screenshot-simple_rules-0.3.0-pre2.png"/>
          </a>
        </div>
          
        <div style="margin: 1em 1em 1em 1em; text-align:center;">
-         <h3>Add advanced filter window</h3>
-         <a href="img/screenshot-add_advanced_filter-0.2.0-pre6.png">
-            <img src="img/thmb-screenshot-add_advanced_filter-0.2.0-pre6.png"/>
+         <h3>Edit advanced rules window</h3>
+         <a href="img/screenshot-advanced_filter-0.3.0-pre2.png">
+            <img src="img/thmb-screenshot-advanced_filter-0.3.0-pre2.png"/>
          </a>
        </div>
        
        <div style="margin: 1em 1em 1em 1em; text-align:center;">
          <h3>Regular expression editor</h3>
-         <a href="img/screenshot-edit_regular_expression-0.2.0-pre6.png">
-            <img src="img/thmb-screenshot-edit_regular_expression-0.2.0-pre6.png"/>
+         <a href="img/screenshot-edit_regular_expression-0.3.0-pre2.png">
+            <img src="img/thmb-screenshot-edit_regular_expression-0.3.0-pre2.png"/>
          </a>
        </div>
     
