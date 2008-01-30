f0a008c281d287f4e35c02f33af5e690bbd34320
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2008-01-30 16:34:58 +0000

    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@426 540001c9-3a26-0410-bb9e-87544be22eba

---
+++ itsucks-core/src/main/java/de/phleisch/app/itsucks/processing/DataProcessorInfo.java
@@ -0,0 +1,68 @@
+/* Copyright (C) 2006-2007 Oliver Mihatsch (banishedknight@users.sf.net)
+ * This is free software distributed under the terms of the
+ * GNU Public License.  See the file COPYING for details. 
+ *
+ * $Id$
+ * Created on 30.01.2008
+ */
+
+package de.phleisch.app.itsucks.processing;
+
+public class DataProcessorInfo {
+
+	public static enum ResumeSupport {
+		NO_RESUME_SUPPORTED,
+		RESUME_SUPPORTED,
+	}
+	
+	public static enum ProcessorType {
+		CONSUMER,
+		FILTER,
+	}
+	
+	public static enum StreamingSupport {
+		SUPPORT_STREAMING,
+		NEED_DATA_AS_WHOLE_CHUNK,
+	}
+	
+	protected ResumeSupport mResumeSupport;
+	protected ProcessorType mProcessorType;
+	protected StreamingSupport mStreamingSupport;
+
+	public DataProcessorInfo(
+			ResumeSupport pResumeSupport,
+			ProcessorType pProcessorType,
+			StreamingSupport pStreamingSupport) {
+		
+		mResumeSupport = pResumeSupport;
+		mProcessorType = pProcessorType;
+		mStreamingSupport = pStreamingSupport;
+	}
+	
+	/**
+	 * Supports this data processor resuming?
+	 */
+	public ResumeSupport getResumeSupport() {
+		return mResumeSupport;
+	}
+	
+	/**
+	 * 'Consumes' (saves, parses etc.) this processor to the data or is it
+	 * only an filter?
+	 * If an chain contains no consumer, the data is not processed.
+	 */
+	public ProcessorType getProcessorType() {
+		return mProcessorType;
+	}
+	
+	/**
+	 * Asks the processor if it needs the data from the data retriever
+	 * in one chunk and not in multiple pieces.
+	 * 
+	 * @return
+	 */
+	public StreamingSupport getStreamingSupport() {
+		return mStreamingSupport;
+	}
+	
+}
