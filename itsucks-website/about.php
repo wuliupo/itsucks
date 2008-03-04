b2e51ed5036ba7a285fc7fa5571feaf816306661
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2008-03-04 22:30:41 +0000

    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@459 540001c9-3a26-0410-bb9e-87544be22eba

--- itsucks-website/about.php
+++ itsucks-website/about.php
@@ -8,37 +8,91 @@ include_once('include/header.php.inc');
       <p class="text">
       
         <h2>Requirements to run ItSucks</h2>
-      	<ul>
+          <ul>
           <li>Java JRE 1.6 or better. 
           You can get it at <a href="http://java.sun.com">http://java.sun.com</a>.</li>
           <li>min. 256 MB RAM (the more the better, java craves for memory)</li>
         </ul>
       
-        <h2>Features of ItSucks Core library</h2>
+        <h2>Features</h2>
+
+        <h3>General Features</h3>
+        <ul>
+          <li>Multithreaded, configurable count of working threads</li>
+          <li>Regular Expression Editor to test expressions on the fly</li>
+          <li>Save/Load download jobs</li>
+        </ul>
+        
+        <h3>HTTP Connection Features</h3>
+        <ul>
+          <li>HTTP/HTTPS supported</li>
+          <li>HTTP Proxy Support (+ proxy authentication)</li>
+          <li>Configurable User Agent</li>
+          <li>Limitation of connections per server</li>
+          <li>Configurable behaviour for HTTP response codes<br>
+          Example: If an server sends 403 (Forbidden) after to many download, a retry + waiting time can be defined.</li>
+          <li>Bandwidth limitation</li>
+        </ul>
+
+        <h2>Rules</h2>
+        <p>
+          ItSucks offers a large variety of filters which can be used to isolate a specific
+          part of an website.  
+        </p>
+        <h3>Simple rules:</h3>
+        <ul>
+          <li>Limitation of link depth<br>
+          Example: To download only two levels after the initial link, set the value to 2.</li>
+          <li>Limitation of links to follow (count)<br>
+          Example: Stop adding new links after finding 5000 links.</li>
+          <li>Limitation of time per job<br>
+          Example: Stop adding new links after 30 minutes.</li>
+          <li>Allowed Hostname filter (regular expression)<br>
+          Example: Define '.*\.google.(de|com)' to allow all subdomains from google.de and google.com.</li>
+          <li>Regular Expression Filter to save only certain filetypes/names on disk<br>
+          Example: Define '.*jpg|.*png' to save only files which ends with 'jpg' or 'png'.</li>
+        </ul>
+        
+        <h3>Special rules:</h3>
+        <ul>
+          <li>File Size filter<br>
+          Example: Only save files on disk which are larger than 100kb.</li>
+        </ul>
+
+        <h3>Advanced Regular Expression Rules:</h3>
+        <ul>
+          <li>A highly customizable filter chain can hold multiple regular expressions. 
+          For every expression, actions can be defined when the regular expression matches a URL
+          and actions to be executed when the expression does not match.<br/>
+          Possible actions are: 
+          follow the URL (Accept), do not follow the URL (Reject), change the priority of the URL</li>
+        </ul>
+
+        <h3>Content filter:</h3>
+        <ul>
+          <li>Content filter for text/html files (regular expression)<br>
+          Example: Only download link if the content contains 'New mail.*arrived'.</li>
+        </ul>
+      
+        <h2>Console</h2>
+        <ul>
+          <li>Start your download templates on the console after creating it with the GUI.</li>
+        </ul>         
+      
+        <h2>Core library and API</h2>
+        <p>
+        	ItSucks is splitted into an backend (core) and frontend part. The backend can be used to implement
+        	own web crawler/spider. License is GPL.
+        </p>
         
         <h3>General Features</h3>
         <ul>
           <li>Designed to support multiple protocols. At this time only http/https is implemented.</li>
           <li>Running multiple crawl jobs simultaniously.</li>
-          <li>Number of working threads for every crawl job is changeable. (Not over the GUI available yet.)</li>
-          <li>Search depth is configurable.</li>
-          <li>Support for individual priority of an URL.</li>
+          <li>Customizable filter chain to filter found urls.</li>
           <li>Customizable processing chain to process the downloaded data.</li>
           <li>Save and load one or multiple Jobs. (serialize / deserialize)</li>
           <li>Fully programmed with Java 1.5 features (generics etc.).</li>
-        </ul>
-        
-        <h3>URL Filter Features</h3>
-        <ul>
-          <li>Allowed hostnames are configurable by regular expressions.
-          <li>Type of files to be downloaded are configurable by regular expressions.</li>
-          <li>A base URL can be set to follow only URL's beginning with this prefix.</li>
-          <li>A highly configurable advanced filter can hold multiple regular expressions. 
-          For every expression, actions can be defined when the regular expression matches the URL
-          and actions to be executed when the expression does not match.<br/>
-          Possible actions are: 
-          follow the URL (Accept), do not follow the URL (Reject), change the priority of the URL</li>
-          <li>A filter can inject additional parameters into the job.</li>
         </ul>
         
         <h3>Event Handling</h3>
@@ -47,19 +101,6 @@ include_once('include/header.php.inc');
           <li>Possibility to filter events by category and type.</li>
         </ul>
   
-        <h2>Features of ItSucks Swing GUI</h2>
-        <ul>
-          <li>Keep track of multiple download jobs.</li>
-          <li>Add and remove download jobs.</li>
-          <li>A integrated regular expression tester to test/debug your regular expression directly.</li>
-          <li>Load and save downloads as templates.</li>
-        </ul>
-        
-        <h2>Features of ItSucks Console</h2>
-        <ul>
-          <li>Start your download templates on the console after creating it with the GUI.</li>
-        </ul>        
-        
         <h2>External libraries/tools used</h2>
         <ul>
           <li><a href="http://www.springframework.org/">Spring Application Framework</a></li>
@@ -69,11 +110,12 @@ include_once('include/header.php.inc');
           + <a href="http://jakarta.apache.org/commons/logging/">Jakarta Commons Logging</a></li>
           <li><a href="http://www.junit.org/">JUnit</a></li>
           <li><a href="http://eclipse.org">Eclipse development platform</a>
-          	<ul>
-          		<li><a href="http://subclipse.tigris.org/">Subclipse plugin (Subversion)</a></li>
-          		<li><a href="http://springide.org/">Spring IDE plugin</a></li>
-          		<li><a href="http://www.slimeuml.de/">Slime UML plugin</a></li>
-          	</ul>
+              <ul>
+                  <li><a href="http://subclipse.tigris.org/">Subclipse plugin (Subversion)</a></li>
+                  <li><a href="http://springide.org/">Spring IDE plugin</a></li>
+                  <li><a href="http://www.slimeuml.de/">Slime UML plugin</a></li>
+                  <li><a href="http://classycleplugin.graf-tec.ch/">Classycle Plugin for Eclipse</a></li>
+              </ul>
           </li>
         </ul>
         
