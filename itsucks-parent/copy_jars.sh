ce4f9495be88b055f0fef56308aa9a5d3f7a063c
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2008-03-05 21:43:08 +0000

    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@461 540001c9-3a26-0410-bb9e-87544be22eba

---
+++ itsucks-parent/copy_jars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TARGET_DIR=$1
+echo "Target directory: $TARGET_DIR"
+
+cp ../itsucks-base/target/itsucks-base*.jar $TARGET_DIR
+cp ../itsucks-core/target/itsucks-core*.jar $TARGET_DIR
+cp ../itsucks-gui/target/itsucks-gui*.jar $TARGET_DIR
+cp ../itsucks-console/target/itsucks-console*.jar $TARGET_DIR
+cp ../itsucks-vmcheck/target/itsucks-vmcheck*.jar $TARGET_DIR
+
