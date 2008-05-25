db446a1149bde9bf88ec7cd97abac3d5712a3e70
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2008-05-25 21:18:51 +0000

    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@522 540001c9-3a26-0410-bb9e-87544be22eba

--- itsucks-website/screenshots.php
+++ itsucks-website/screenshots.php
@@ -8,31 +8,59 @@ include_once('include/header.php.inc');
   
        <div style="margin:1em 1em 1em 1em; text-align:center;">
          <h3>Main window + add download window</h3>
-         <a href="img/screenshot-main-0.3.0-pre2.png">
-            <img style="vertical-align:middle;" src="img/thmb-screenshot-main-0.3.0-pre2.png"/>
+         <a href="img/main-0.3.0-pre7.png">
+            <img style="vertical-align:middle;" src="img/thmb_main-0.3.0-pre7.png"/>
          </a>
        </div>
        
+       <div style="margin:1em 1em 1em 1em; text-align:center;">
+         <h3>Basic Settings</h3>
+         <a href="img/basic_settings-0.3.0-pre7.png">
+            <img style="vertical-align:middle;" src="img/thmb_basic_settings-0.3.0-pre7.png"/>
+         </a>
+       </div>
+       
+       <div style="margin:1em 1em 1em 1em; text-align:center;">
+         <h3>Cookie Settings</h3>
+         <a href="img/cookie-0.3.0-pre7.png">
+            <img style="vertical-align:middle;" src="img/thmb_cookie-0.3.0-pre7.png"/>
+         </a>
+       </div>       
+       
        <div style="margin: 1em 1em 1em 1em; text-align:center;">
-         <h3>Edit simple rules window</h3>
-         <a href="img/screenshot-simple_rules-0.3.0-pre2.png">
-            <img src="img/thmb-screenshot-simple_rules-0.3.0-pre2.png"/>
+         <h3>Simple rules</h3>
+         <a href="img/simple_rules-0.3.0-pre7.png">
+            <img src="img/thmb_simple_rules-0.3.0-pre7.png"/>
          </a>
        </div>
          
        <div style="margin: 1em 1em 1em 1em; text-align:center;">
-         <h3>Edit advanced rules window</h3>
-         <a href="img/screenshot-advanced_filter-0.3.0-pre2.png">
-            <img src="img/thmb-screenshot-advanced_filter-0.3.0-pre2.png"/>
+         <h3>Advanced Regexp rules</h3>
+         <a href="img/regexp_rules-0.3.0-pre7.png">
+            <img src="img/thmb_regexp_rules-0.3.0-pre7.png"/>
          </a>
        </div>
        
        <div style="margin: 1em 1em 1em 1em; text-align:center;">
+         <h3>Content filter</h3>
+         <a href="img/content_filter-0.3.0-pre7.png">
+            <img src="img/thmb_content_filter-0.3.0-pre7.png"/>
+         </a>
+       </div>       
+       
+       <div style="margin: 1em 1em 1em 1em; text-align:center;">
          <h3>Regular expression editor</h3>
-         <a href="img/screenshot-edit_regular_expression-0.3.0-pre2.png">
-            <img src="img/thmb-screenshot-edit_regular_expression-0.3.0-pre2.png"/>
+         <a href="img/regexp_editor-0.3.0-pre7.png">
+            <img src="img/thmb_regexp_editor-0.3.0-pre7.png"/>
          </a>
        </div>
+    
+       <div style="margin: 1em 1em 1em 1em; text-align:center;">
+         <h3>Help</h3>
+         <a href="img/help-0.3.0-pre7.png">
+            <img src="img/thmb_help-0.3.0-pre7.png"/>
+         </a>
+       </div>    
     
     </div>
     
