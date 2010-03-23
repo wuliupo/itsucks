20b783c391fb812db2e9d330f720bfe528493820
banishedknight <banishedknight@540001c9-3a26-0410-bb9e-87544be22eba>
2010-03-23 22:19:19 +0000

    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@598 540001c9-3a26-0410-bb9e-87544be22eba

---
+++ itsucks-website/new/index.php
@@ -0,0 +1,29 @@
+<?
+$selected_menu_entry = 'home';
+
+include_once('include/header.php.inc');
+?>
+
+    <div id="info1">
+    	Info1alsjdflajdlf laksdjf lasjdf lasjd flajsdf lasjdf laksf alskdfj asdlkfj asldfj<br/>
+    	Info1alsjdflajdlf laksdjf lasjdf lasjd flajsdf lasjdf laksf alskdfj asdlkfj asldfj  
+    </div>
+    <div id="info2">
+    	Info2alsjdflajdlf laksdjf lasjdf lasjd flajsdf lasjdf laksf alskdfj asdlkfj asldfj<br/>
+    	Info2alsjdflajdlf laksdjf lasjdf lasjd flajsdf lasjdf laksf alskdfj asdlkfj asldfj  
+    </div>
+    
+    <div id="main">
+      <p class="text"><h3>Welcome to the official homepage of ItSucks.</h3><p>
+      <p class="text">
+      This project is a java web spider (web crawler) with the ability to download (and resume) files. It is also highly customizable with regular expressions and download templates.
+      </p>
+      <p class="text">  
+      The application also provides a swing GUI and a console interface. 
+      All backend functionalities are also available in a separate library, they can be easily used for other projects.
+      </p>
+    </div>    
+    
+<?
+include_once('include/footer.php.inc');
+?>
