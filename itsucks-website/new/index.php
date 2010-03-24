e6587d54dbcaaa3a62d061968c9e4d4ce4a7c142
banishedKnight <banishedKnight@540001c9-3a26-0410-bb9e-87544be22eba>
2010-03-24 17:34:23 +0000

    git-svn-id: http://svn.code.sf.net/p/itsucks/code/trunk@599 540001c9-3a26-0410-bb9e-87544be22eba

--- itsucks-website/new/index.php
+++ itsucks-website/new/index.php
@@ -5,8 +5,22 @@ include_once('include/header.php.inc');
 ?>
 
     <div id="info1">
-    	Info1alsjdflajdlf laksdjf lasjdf lasjd flajsdf lasjdf laksf alskdfj asdlkfj asldfj<br/>
-    	Info1alsjdflajdlf laksdjf lasjdf lasjd flajsdf lasjdf laksf alskdfj asdlkfj asldfj  
+		<h3>Features:</h3>
+		<ul>
+			<li>Multithreading</li>
+			<li>Regular Expressions</li>
+			<li>Save/Load download jobs</li>
+			<li>Online Help</li>
+			<li>HTTP/HTTPS supported</li>
+			<li>HTTP Proxy Support</li>
+			<li>HTTP Authentication</li>
+			<li>Cookie Support</li>
+			<li>Configurable User Agent</li>
+			<li>Limitation of connections</li>
+			<li>Configurable behaviour for HTTP response codes</li>
+			<li>Bandwidth limitation</li>
+			<li>GZip compression</li>
+		<ul>
     </div>
     <div id="info2">
     	Info2alsjdflajdlf laksdjf lasjdf lasjd flajsdf lasjdf laksf alskdfj asdlkfj asldfj<br/>
@@ -14,15 +28,32 @@ include_once('include/header.php.inc');
     </div>
     
     <div id="main">
-      <p class="text"><h3>Welcome to the official homepage of ItSucks.</h3><p>
-      <p class="text">
-      This project is a java web spider (web crawler) with the ability to download (and resume) files. It is also highly customizable with regular expressions and download templates.
-      </p>
-      <p class="text">  
-      The application also provides a swing GUI and a console interface. 
-      All backend functionalities are also available in a separate library, they can be easily used for other projects.
-      </p>
-    </div>    
+        <h3>About ItSucks</h3>
+        <p>This project is a java web spider (web crawler) with the ability to download (and resume) files. It is also highly customizable with regular expressions and download templates.</p>
+        <p>The application also provides a swing GUI and a console interface. All backend functionalities are also available in a separate library, they can be easily used for other projects.</p>
+        
+        <div class="recentNews">
+          <p class="text">
+	        <h3 style="margin-bottom: 0;">Version 0.4.0 is released</h3>
+	        <span class="subHeader">2009-11-26</span>
+	        <p>
+			This release contains a complete rewrite of the resume logic. Resuming can be disabled over the GUI.<br>
+			New is also the support for referrals. Many sites use them to ensure you are not using deeplinks. (They can be disabled over the GUI though.)<br>
+			Also a issue is fixed with parameters in redirects (403). (Bug 2680409, thanks for the detailed bug report)
+			</p>
+			Other changes:
+			<ul>
+			<li>Switched to JAXB 2.0</li>
+			<li>Updated Maven configuration (2.2)</li>
+			<li>Refactoring HttpRetriever Interface</li>
+			<li>Build source jars</li>
+			</ul>		
+		</p>
+		<p>
+			Detailed changelog can be found here: <a href="https://sourceforge.net/projects/itsucks/files/itsucks/itsucks-releasenotes-0.4.0.txt/view"> ChangeLog 0.4.0</a>
+		</p>
+        </div>
+    </div>
     
 <?
 include_once('include/footer.php.inc');
